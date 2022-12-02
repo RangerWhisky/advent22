62f6907af4420a23ce5582db00949c37f8b51619
Rob Wallis <rangerwhisky@outlook.com>
2022-12-02 08:07:16 +0000

    Score component complete

--- day2/day2.go
+++ day2/day2.go
@@ -1,1 +1,15 @@
 package day2
+
+func GetResponseScore(input string) int {
+	response := input[len(input)-1]
+	value := 0
+	switch response {
+	case 'X':
+		value = 1
+	case 'Y':
+		value = 2
+	case 'Z':
+		value = 3
+	}
+	return value
+}
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -2,10 +2,26 @@ package day2
 
 import "testing"
 
-func TestGetResponseScore(t *testing.T) {
+func TestGetRockResponseScore(t *testing.T) {
 	testInput := "A X"
 	score := GetResponseScore(testInput)
 	if score != 1 {
+		t.Errorf("Response score is = %d, need 1", score)
+	}
+}
+
+func TestGetPaperResponseScore(t *testing.T) {
+	testInput := "A Y"
+	score := GetResponseScore(testInput)
+	if score != 2 {
+		t.Errorf("Response score is = %d, need 1", score)
+	}
+}
+
+func TestGetScissorsResponseScore(t *testing.T) {
+	testInput := "A Z"
+	score := GetResponseScore(testInput)
+	if score != 3 {
 		t.Errorf("Response score is = %d, need 1", score)
 	}
 }
