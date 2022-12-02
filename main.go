9a923111ba67b5ed067d23000d850ca500178317
Rob Wallis <rangerwhisky@outlook.com>
2022-12-02 09:48:57 +0000

    Correct answer for day 2

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 func main() {
 	fmt.Println("Day 1")
 
-	fmt.Printf("Biggest pack is %d ", day1.Easy("day1/input"))
-	fmt.Printf("Total of biggest 3 packs is %d ", day1.Bonus("day1/input"))
+	fmt.Printf("Biggest pack is %d \n", day1.Easy("day1/input"))
+	fmt.Printf("Total of biggest 3 packs is %d \n", day1.Bonus("day1/input"))
 
 	fmt.Println("\nDay 2")
-	fmt.Printf("Score for strategy card is %d", day2.Easy("day2/input"))
+	fmt.Printf("Score for strategy card is %d\n", day2.Easy("day2/input"))
+	fmt.Printf("Score for strategy card with target result %d\n", day2.Bonus("day2/input"))
 
 	fmt.Println("\nDone!")
 }
