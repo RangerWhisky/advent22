e732d37834d755fbf2f23cc8160282e69745c4d4
Rob Wallis <rangerwhisky@outlook.com>
2022-12-03 13:55:36 +0000

    Get dupe letter working with n squared algorithm

--- day3/day3.go
+++ day3/day3.go
@@ -1,5 +1,21 @@
 package day3
 
 func GetDuplicatedLetters(backpack string) byte {
-	return 'a'
+	dupeLetter := byte('a')
+	comparmentSize := getCompartmentSize(backpack)
+
+	for i := 0; i < getCompartmentSize(backpack); i++ {
+		matchCharacter := byte(backpack[i])
+		for j := comparmentSize; j < len(backpack); j++ {
+			if matchCharacter == byte(backpack[j]) {
+				dupeLetter = matchCharacter
+			}
+		}
+	}
+
+	return dupeLetter
+}
+
+func getCompartmentSize(backpack string) int {
+	return len(backpack) / 2
 }
