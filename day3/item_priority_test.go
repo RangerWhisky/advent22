06043b5b1b5aa953534ae17dba1be7e8946e063b
Rob Wallis <rangerwhisky@outlook.com>
2022-12-03 13:39:27 +0000

    Failing test for getting Uppercase letter priority

--- day3/item_priority_test.go
+++ day3/item_priority_test.go
@@ -9,3 +9,19 @@ func TestGetItemPriority(t *testing.T) {
 		t.Errorf("Item Priority should be 1 for a but is %d", itemPriority)
 	}
 }
+
+func TestGetItemPriorityEndRange(t *testing.T) {
+	itemPriority := GetItemPriority('z')
+
+	if itemPriority != 26 {
+		t.Errorf("Item Priority should be 26 for z but is %d", itemPriority)
+	}
+}
+
+func TestGetCapitalItemPriority(t *testing.T) {
+	itemPriority := GetItemPriority('A')
+
+	if itemPriority != 27 {
+		t.Errorf("Item Priority should be 27 for A but is %d", itemPriority)
+	}
+}
