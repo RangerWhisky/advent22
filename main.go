077a37096f32fef076415e9bbc9281288bdc4d00
Rob Wallis <rangerwhisky@outlook.com>
2022-12-04 10:40:03 +0000

    Correct answer for 1 star

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"localhost/advent22/day1"
 	"localhost/advent22/day2"
 	"localhost/advent22/day3"
+	"localhost/advent22/day4"
 )
 
 func main() {
@@ -20,6 +21,9 @@ func main() {
 	fmt.Println("\nDay 3")
 	fmt.Printf("Total priority for incorrect packing is %d\n", day3.PartOne("day3/input"))
 	fmt.Printf("Total badge priority for groups is %d\n", day3.PartTwo("day3/input"))
+
+	fmt.Println("\nDay 4")
+	fmt.Printf("Count of overlapping sections is %d\n", day4.PartOne("day4/input"))
 
 	fmt.Println("\nDone!")
 }
