48e2fc00091f5cdbbc6dc4dc7a5c59ac15d8cca8
Rob Wallis <rangerwhisky@outlook.com>
2022-12-05 08:00:27 +0000

    Rigorous test for checking stack contents

--- day5/cargo_stack_test.go
+++ day5/cargo_stack_test.go
@@ -10,4 +10,11 @@ func TestCreateStackFromBytes(t *testing.T) {
 	if stack.height != 3 {
 		t.Errorf("Stack height set to %d but should be 3", stack.height)
 	}
+
+	for i := 0; i < len(testInput); i++ {
+		if testInput[i] != stack.cargo[i] {
+			t.Errorf("Non matching stack descriptions - input %q - saved %q", testInput, stack.cargo)
+			t.FailNow()
+		}
+	}
 }
