94fd740a0e59ffa8dd2d8c61b8c8012d75982cca
Rob Wallis <rangerwhisky@outlook.com>
2022-12-05 20:37:30 +0000

    Working without unit tests - issues with Pt1

--- day5/day5.go
+++ day5/day5.go
@@ -1,33 +1,43 @@
 package day5
 
 import (
+	"fmt"
 	file_input "localhost/advent22/utils"
 )
 
 func PartOne(filepath string) string {
 	crateStarter := []byte{}
 	elfInput := file_input.Read_file(filepath)
+	stackCountLine := 0
 	instructionStart := 0
 
 	for i := 0; i < len(elfInput); i++ {
-		if len(elfInput[i]) != 0 {
+		if elfInput[i][1] != '1' {
 			array := []byte(elfInput[i])
 			crateStarter = append(crateStarter, array...)
 			crateStarter = append(crateStarter, '\n')
 		} else {
-			instructionStart = i + 1
+			stackCountLine = i
+			instructionStart = i + 2
 			break
 		}
 	}
 
-	stacks := GetCargoStackInput(string(crateStarter))
+	stacks := GetCargoStackInput(string(elfInput[stackCountLine]))
+
+	tops := []byte{}
+	for i := 0; i < len(stacks); i++ {
+		tops = append(tops, GetTop(stacks[i]))
+	}
+	fmt.Printf("%q\n", tops)
+
+	tops = []byte{}
 
 	for i := instructionStart; i < len(elfInput); i++ {
 		quantity, source, dest := GetInstruction(elfInput[i])
-		MoveCargo(&stacks[source], &stacks[dest], quantity)
+		MoveCargo(&stacks[source-1], &stacks[dest-1], quantity)
 	}
 
-	tops := []byte{}
 	for i := 0; i < len(stacks); i++ {
 		tops = append(tops, GetTop(stacks[i]))
 	}
