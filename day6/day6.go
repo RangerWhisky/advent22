9b192a9c75e1295f293fd6efc71976d1e9f98e79
Rob Wallis <rangerwhisky@outlook.com>
2022-12-06 05:56:04 +0000

    False test passing

--- day6/day6.go
+++ day6/day6.go
@@ -1,5 +1,16 @@
 package day6
 
+import "bytes"
+
 func IsStartMarker(signal []byte) bool {
-	return true
+	isSignal := true
+
+	for i := 1; i < len(signal); i++ {
+		var subset []byte = signal[0:i]
+		var itemToCheck []byte = signal[i:i]
+		if bytes.Contains(subset, itemToCheck) {
+			isSignal = false
+		}
+	}
+	return isSignal
 }
