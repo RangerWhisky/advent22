cd9a64eec8da2f7d02cb7677972686fc1b62222d
Rob Wallis <rangerwhisky@outlook.com>
2022-12-06 06:07:48 +0000

    Passing tests for uniqueness

--- day6/day6.go
+++ day6/day6.go
@@ -1,14 +1,18 @@
 package day6
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func IsStartMarker(signal []byte) bool {
 	isSignal := true
 
 	for i := 1; i < len(signal); i++ {
-		var subset []byte = signal[0:i]
-		var itemToCheck []byte = signal[i:i]
+		var subset []byte = signal[:i]
+		var itemToCheck []byte = signal[i : i+1]
 		if bytes.Contains(subset, itemToCheck) {
+			fmt.Printf("Is %q in %q", itemToCheck, subset)
 			isSignal = false
 		}
 	}
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -1,12 +1,23 @@
 package day6
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsStartMarker(t *testing.T) {
 	testData := []byte("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-	startMarker := IsStartMarker(testData[0:4])
+
+	subsetToCheck := testData[0:4]
+	startMarker := IsStartMarker(subsetToCheck)
 
 	if startMarker != false {
-		t.Errorf("Found a start marker where there is none")
+		t.Errorf("Found a start marker where there is none for %q", subsetToCheck)
 	}
+
+	subsetToCheck = testData[4:8]
+	startMarker = IsStartMarker(subsetToCheck)
+	if startMarker != true {
+		t.Errorf("Failed to find a valid start marker for %q", subsetToCheck)
+	}
+
 }
