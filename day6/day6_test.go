e2ced23bfc96def6e7dd1f35c80763744fbc3e87
Rob Wallis <rangerwhisky@outlook.com>
2022-12-06 06:10:06 +0000

    Added tougher test for boundary check

--- day6/day6_test.go
+++ day6/day6_test.go
@@ -21,3 +21,21 @@ func TestIsStartMarker(t *testing.T) {
 	}
 
 }
+
+func TestCornerCaseStartMarker(t *testing.T) {
+	testData := []byte("mjqjpqmgj")
+
+	subsetToCheck := testData[0:4]
+	startMarker := IsStartMarker(subsetToCheck)
+
+	if startMarker != false {
+		t.Errorf("Found a start marker where there is none for %q", subsetToCheck)
+	}
+
+	subsetToCheck = testData[4:8]
+	startMarker = IsStartMarker(subsetToCheck)
+	if startMarker != true {
+		t.Errorf("Failed to find a valid start marker for %q", subsetToCheck)
+	}
+
+}
