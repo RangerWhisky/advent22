a69c2d0e327afe07fd4e288f78506f79a9484d33
Rob Wallis <rangerwhisky@outlook.com>
2022-12-07 06:11:53 +0000

    Passing test for simplest imaginable directory

--- day7/day7.go
+++ day7/day7.go
@@ -1,5 +1,21 @@
 package day7
 
-func GetDirectorySize([][]byte) int {
-	return 0
+import (
+	"strconv"
+	"strings"
+)
+
+func GetDirectorySize(contents [][]byte) int {
+	size := 0
+	for i := 0; i < len(contents); i++ {
+		if IsDir(contents[i]) {
+			size += GetDirectorySize(contents[i:])
+		} else if IsFile(contents[i]) {
+			parts := strings.Split(string(contents[i]), " ")
+			fileSize, _ := strconv.Atoi(parts[0])
+			size += fileSize
+		}
+
+	}
+	return size
 }
