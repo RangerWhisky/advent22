9df79cc453b7236d2e264103a562ddc3d9a9c801
Rob Wallis <rangerwhisky@outlook.com>
2022-12-08 15:22:19 +0000

    Framework for day1 code

--- day7/day7.go
+++ day7/day7.go
@@ -1,10 +1,20 @@
 package day7
 
-func PartOne(filepath string) {
+import (
+	file_input "localhost/advent22/utils"
+)
+
+func PartOne(filepath string) int {
 	// get file contents
 	// when cd command is found
 	//    add new entry to the Filesystem
 	//    fill out the directory details
 	//
 
+	elfInput := file_input.Read_file(filepath)
+
+	for i := 0; i < len(elfInput); i++ {
+
+	}
+	return 0
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -1,1 +1,12 @@
 package day7
+
+import (
+	"testing"
+)
+
+func TestPartOneSolution(t *testing.T) {
+	solution := PartOne("./simplified_example.txt")
+	if solution != 10 {
+		t.Errorf("Start of packet marker complete after %d characters but should be %d", solution, 10)
+	}
+}
