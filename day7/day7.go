3fdad0d3bb2681b72f5527fd9a8c8221d0b53936
Rob Wallis <rangerwhisky@outlook.com>
2022-12-08 14:33:29 +0000

    Pathfinding a solution in code, next to test

--- day7/day7.go
+++ day7/day7.go
@@ -1,33 +1,10 @@
 package day7
 
-import (
-	"strconv"
-	"strings"
-)
+func PartOne(filepath string) {
+	// get file contents
+	// when cd command is found
+	//    add new entry to the Filesystem
+	//    fill out the directory details
+	//
 
-type Directory struct {
-	subdirs []string
-	size    int
-}
-
-func GetDirectorySize(contents [][]byte) int {
-	dir := ParseDirectory(contents)
-	return dir.size
-}
-
-func ParseDirectory(contents [][]byte) Directory {
-	var dir = new(Directory)
-
-	for i := 0; i < len(contents); i++ {
-		if IsDir(contents[i]) {
-			parts := strings.Split(string(contents[i]), " ")
-			dir.subdirs = append(dir.subdirs, parts[1])
-		} else if IsFile(contents[i]) {
-			parts := strings.Split(string(contents[i]), " ")
-			fileSize, _ := strconv.Atoi(parts[0])
-			dir.size += fileSize
-		}
-
-	}
-	return *dir
 }
