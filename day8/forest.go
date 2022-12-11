dddad33c2b98b24947fd476483820e52c64ecd68
Rob Wallis <rangerwhisky@outlook.com>
2022-12-11 21:50:43 +0000

    Tests and code for visibility check from each direction

--- day8/forest.go
+++ day8/forest.go
@@ -5,7 +5,8 @@ import (
 )
 
 type Forest struct {
-	mapData utils.IntMap
+	mapData    utils.IntMap
+	visibility [][]bool
 }
 
 func InitialiseForestFromFile(filepath string) Forest {
@@ -17,6 +18,83 @@ func InitialiseForestFromFile(filepath string) Forest {
 	forest.mapData = utils.InitialiseMap(file)
 
 	return forest
+}
+
+func AssessVisibility(forest *Forest) {
+	// trade off of memory usage vs performance
+	// for performance, store the top tree height observed from each direction, then iterate all
+	// tree points and compare against neighbours only
+
+	// for memory, for each point compare against neighbours until failure
+
+	height := utils.GetHeight(&forest.mapData)
+	width := utils.GetHeight(&forest.mapData)
+
+	var visibilityMap [][]bool
+
+	for i := 0; i < height; i++ {
+		var boolArray []bool
+		for j := 0; j < width; j++ {
+			boolArray = append(boolArray, IsTreeVisible(forest, i, j))
+		}
+		visibilityMap = append(visibilityMap, boolArray)
+	}
+
+	forest.visibility = visibilityMap
+}
+
+func IsTreeVisible(forest *Forest, height int, width int) bool {
+	treeHeight := utils.GetValue(&forest.mapData, width, height)
+
+	// from north
+	visibleFromDirection := IsVisibleFromNorth(forest, width, height, treeHeight) ||
+		IsVisibleFromSouth(forest, width, height, treeHeight) ||
+		IsVisibleFromWest(forest, width, height, treeHeight) ||
+		IsVisibleFromEast(forest, width, height, treeHeight)
+
+	return visibleFromDirection
+}
+
+func IsVisibleFromNorth(forest *Forest, height int, width int, treeHeight int) bool {
+	visibleFromDirection := true
+	for i := 0; i < height && visibleFromDirection; i++ {
+		if utils.GetValue(&forest.mapData, i, width) >= treeHeight {
+			visibleFromDirection = false
+		}
+	}
+	return visibleFromDirection
+}
+
+func IsVisibleFromSouth(forest *Forest, height int, width int, treeHeight int) bool {
+	visibleFromDirection := true
+	maxHeight := utils.GetHeight(&forest.mapData)
+	for i := height + 1; i < maxHeight && visibleFromDirection; i++ {
+		if utils.GetValue(&forest.mapData, i, width) >= treeHeight {
+			visibleFromDirection = false
+		}
+	}
+	return visibleFromDirection
+}
+
+func IsVisibleFromWest(forest *Forest, height int, width int, treeHeight int) bool {
+	visibleFromDirection := true
+	for i := 0; i < width && visibleFromDirection; i++ {
+		if utils.GetValue(&forest.mapData, height, i) >= treeHeight {
+			visibleFromDirection = false
+		}
+	}
+	return visibleFromDirection
+}
+
+func IsVisibleFromEast(forest *Forest, height int, width int, treeHeight int) bool {
+	visibleFromDirection := true
+	maxWidth := utils.GetWidth(&forest.mapData)
+	for i := width + 1; i < maxWidth && visibleFromDirection; i++ {
+		if utils.GetValue(&forest.mapData, height, i) >= treeHeight {
+			visibleFromDirection = false
+		}
+	}
+	return visibleFromDirection
 }
 
 func getTreeCount(forest *Forest) int {
--- day8/forest_test.go
+++ day8/forest_test.go
@@ -13,3 +13,58 @@ func TestInitialiseForest(t *testing.T) {
 		t.Errorf("Got %d expected %d", treeCount, 25)
 	}
 }
+
+func TestAssessVisibility(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
+
+	AssessVisibility(&forest)
+}
+
+func TestVisibleFromNorth(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
+
+	if !IsVisibleFromNorth(&forest, 1, 1, 5) {
+		t.Errorf("False negative")
+	}
+
+	if IsVisibleFromNorth(&forest, 2, 2, 3) {
+		t.Errorf("False positive")
+	}
+
+}
+
+func TestVisibleFromSouth(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
+
+	if !IsVisibleFromSouth(&forest, 3, 2, 5) {
+		t.Errorf("False negative")
+	}
+
+	if IsVisibleFromSouth(&forest, 3, 1, 3) {
+		t.Errorf("False positive")
+	}
+
+}
+
+func TestVisibleFromWest(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
+
+	if !IsVisibleFromWest(&forest, 1, 1, 5) {
+		t.Errorf("False negative")
+	}
+
+	if IsVisibleFromWest(&forest, 2, 1, 5) {
+		t.Errorf("False positive")
+	}
+}
+func TestVisibleFromEast(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
+
+	if !IsVisibleFromEast(&forest, 0, 3, 7) {
+		t.Errorf("False negative")
+	}
+
+	if IsVisibleFromEast(&forest, 1, 3, 1) {
+		t.Errorf("False positive")
+	}
+}
