de85ac18719fc705b0f5780b9404460960ed0504
Rob Wallis <rangerwhisky@outlook.com>
2022-12-11 20:56:42 +0000

    Simple test for getting forest data

--- day8/forest.go
+++ day8/forest.go
@@ -1,1 +1,24 @@
 package day8
+
+import (
+	utils "localhost/advent22/utils"
+)
+
+type Forest struct {
+	mapData utils.IntMap
+}
+
+func InitialiseForestFromFile(filepath string) Forest {
+
+	var forest Forest
+
+	file := utils.Read_file("./simplified_example.txt")
+
+	forest.mapData = utils.InitialiseMap(file)
+
+	return forest
+}
+
+func getTreeCount(forest *Forest) int {
+	return utils.GetHeight(&forest.mapData) * utils.GetWidth(&forest.mapData)
+}
--- day8/forest_test.go
+++ day8/forest_test.go
@@ -1,7 +1,15 @@
 package day8
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestInitialiseForest(t *testing.T) {
+	forest := InitialiseForestFromFile("./simplified_example.txt")
 
+	treeCount := getTreeCount(&forest)
+
+	if treeCount != 25 {
+		t.Errorf("Got %d expected %d", treeCount, 25)
+	}
 }
