7ee4b9ef13f5fe90bc7bdb82cb1f56ad8a3d0495
Rob Wallis <rangerwhisky@outlook.com>
2022-12-13 19:40:20 +0000

    passing tests for L and R parsing

--- day9/movement_instructions.go
+++ day9/movement_instructions.go
@@ -1,5 +1,24 @@
 package day9
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Decode(instruction string) (int, int) {
-	return 0, 0
+
+	heightDiff := 0
+	widthDiff := 0
+
+	stringParts := strings.Split(instruction, " ")
+	magnitude, _ := strconv.Atoi(stringParts[1])
+
+	switch stringParts[0] {
+	case "R":
+		widthDiff = magnitude
+	case "L":
+		widthDiff = -magnitude
+	}
+
+	return heightDiff, widthDiff
 }
--- day9/movement_instructions_test.go
+++ day9/movement_instructions_test.go
@@ -11,3 +11,13 @@ func TestDecodeRight(t *testing.T) {
 		t.Error()
 	}
 }
+
+func TestDecodeLeft(t *testing.T) {
+	heightDiff, widthDiff := Decode("L 4")
+	if heightDiff != 0 {
+		t.Error()
+	}
+	if widthDiff != -4 {
+		t.Error()
+	}
+}
