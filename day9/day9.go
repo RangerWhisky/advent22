4a2a61b713a679f691bbb39bb41f2456aeecd972
Rob Wallis <rangerwhisky@outlook.com>
2022-12-13 20:46:39 +0000

    Code builds but tests not passing

--- day9/day9.go
+++ day9/day9.go
@@ -18,7 +18,7 @@ func DayOne(filepath string) int {
 		headPosition.width += moveW
 		positions := GetTailPositions(tailPosition, headPosition)
 		for _, p := range positions {
-			utils.MarkMap(&visitMap, p.height, p.width)
+			utils.MarkMap(&visitMap, p.height-1, p.width-1)
 		}
 	}
 
@@ -26,7 +26,33 @@ func DayOne(filepath string) int {
 }
 
 func GetTailPositions(tail Coordinate, head Coordinate) []Coordinate {
-	var positions []Coordinate
-	positions = append(positions, Coordinate{1, 1})
+	positions := []Coordinate{tail}
+	diffH := head.height - tail.height
+	diffW := head.width - tail.width
+
+	// upwards movement case
+	for h := 1; h < diffH; h++ {
+		tailPosition := Coordinate{head.height - h, head.width}
+		positions = append(positions, tailPosition)
+	}
+
+	// downwards movement case
+	for h := -1; h > diffH; h-- {
+		tailPosition := Coordinate{head.height - h, head.width}
+		positions = append(positions, tailPosition)
+	}
+
+	// rightwards movement case
+	for w := 1; w < diffW; w++ {
+		tailPosition := Coordinate{head.height, head.width - w}
+		positions = append(positions, tailPosition)
+	}
+
+	// rightwards movement case
+	for w := -1; w > diffW; w-- {
+		tailPosition := Coordinate{head.height, head.width - w}
+		positions = append(positions, tailPosition)
+	}
+
 	return positions
 }
