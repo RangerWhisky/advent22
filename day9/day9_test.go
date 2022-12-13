2e871076127362d18b43a1ba560c35189fbbffb3
Rob Wallis <rangerwhisky@outlook.com>
2022-12-13 21:07:38 +0000

    Passing test for tail movement

--- day9/day9_test.go
+++ day9/day9_test.go
@@ -42,3 +42,47 @@ func TestGetTailPositionsWithRightChange(t *testing.T) {
 		}
 	}
 }
+
+func TestDiagonalTailPositionsChange(t *testing.T) {
+
+	tail := Coordinate{2, 2}
+
+	head := Coordinate{4, 3}
+
+	positions := GetTailPositions(tail, head)
+
+	if len(positions) < 1 {
+		t.Errorf("Too few tail positions")
+	}
+
+	// count back from head to tail to check our positions
+
+	for i, position := range positions {
+		expectedCoordinate := Coordinate{3 - i, 3}
+		if position != expectedCoordinate {
+			t.Errorf("Expected %q, got %q at %d", expectedCoordinate, position, i)
+		}
+	}
+}
+
+func TestBigNegativeDiagonalChange(t *testing.T) {
+
+	tail := Coordinate{4, 4}
+
+	head := Coordinate{1, 3}
+
+	positions := GetTailPositions(tail, head)
+
+	if len(positions) < 1 {
+		t.Errorf("Too few tail positions")
+	}
+
+	// count back from head to tail to check our positions
+
+	for i, position := range positions {
+		expectedCoordinate := Coordinate{2 + i, 3}
+		if position != expectedCoordinate {
+			t.Errorf("Expected %q, got %q at %d", expectedCoordinate, position, i)
+		}
+	}
+}
