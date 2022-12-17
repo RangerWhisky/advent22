67129e792f1ab871068c218b3ba0b93d59ef734b
Rob Wallis <rangerwhisky@outlook.com>
2022-12-17 12:02:47 +0000

    Multiline CRT check passing

--- day10/crt.go
+++ day10/crt.go
@@ -1,8 +1,15 @@
 package day10
 
+const LineLength int = 40
+
 func IsPixelLit(cycle int, registerValue int) bool {
-	if cycle >= registerValue-1 && cycle <= registerValue+1 {
+	width := getWidthFromCycle(cycle)
+	if width >= registerValue-1 && width <= registerValue+1 {
 		return true
 	}
 	return false
+}
+
+func getWidthFromCycle(cycle int) int {
+	return ((cycle - 1) % LineLength) + 1
 }
--- day10/crt_test.go
+++ day10/crt_test.go
@@ -5,13 +5,11 @@ import "testing"
 func TestCrtOff(t *testing.T) {
 	cycle := 7
 	registerX := 11
-
 	if IsPixelLit(cycle, registerX) {
 		t.Error()
 	}
 
 	registerX = 5
-
 	if IsPixelLit(cycle, registerX) {
 		t.Error()
 	}
@@ -20,19 +18,29 @@ func TestCrtOff(t *testing.T) {
 func TestCrtOn(t *testing.T) {
 	cycle := 4
 	registerX := 3
-
 	if !IsPixelLit(cycle, registerX) {
 		t.Error()
 	}
 
 	registerX = 4
-
 	if !IsPixelLit(cycle, registerX) {
 		t.Error()
 	}
 
 	registerX = 5
+	if !IsPixelLit(cycle, registerX) {
+		t.Error()
+	}
+}
 
+func TestCrtLineTwo(t *testing.T) {
+	cycle := 44
+	registerX := 3
+	if !IsPixelLit(cycle, registerX) {
+		t.Error()
+	}
+
+	registerX = 5
 	if !IsPixelLit(cycle, registerX) {
 		t.Error()
 	}
